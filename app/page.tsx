40a786ec521d4df31c3ddbf13e818d3debff2f04
Nuhamin <ngulilat@myfuse1.education>
2026-06-13 05:32:19 -0400

    added 3d models and about page and some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+import HeroImage from "@/public/hero-image.png";
+
 export default function Home() {
-  return <h1>Hello from the Home page!</h1>;
+  return (
+    <main>
+      <section className="flex flex-col-reverse items-center justify-between gap-8 px-6 py-12 mx-auto md:flex-row max-w-7xl">
+        <div className="flex-1 space-y-6">
+          <p className="hidden text-sm text-gray-600 uppercase md:block">
+            Your go-to platform for 3D printing files
+          </p>
+          <h1 className="text-4xl font-bold md:text-5xl">
+            Discover what is possible with 3D Printing
+          </h1>
+          <p className="text-lg text-gray-600">
+            Join our community of creators and explore a vast library of
+            user-submitted models.
+          </p>
+
+          <div className="flex gap-4">
+            <Link
+              href="/3d-models"
+              className="px-6 py-3 text-black transition duration-100 bg-white border-2 border-black hover:bg-black hover:text-white"
+            >
+              Browse Models
+            </Link>
+          </div>
+        </div>
+        <Image
+          src={HeroImage}
+          alt="Hero Image"
+          width={350}
+          height={350}
+          className="w-[350px] h-auto rounded-lg"
+        />
+      </section>
+    </main>
+  );
 }
